2ee8ea7de6025cb2bf04fa2f6bda077727fbb1f5
dstefan97 <46425878+dstefan97@users.noreply.github.com>
2020-03-29 19:09:23 +0300

    Initial commit

---
+++ PrimeService.Tests/BowlingGameTest.cs
@@ -0,0 +1,169 @@
+﻿using NUnit.Framework;
+using Prime.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prime.UnitTests.Services
+{
+    public class BowlingGameTest 
+    {
+        [SetUp]
+        public void Setup()
+        {
+            _game = new BowlingGame();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _game = null;
+        }
+
+        private BowlingGame _game;
+
+        private void RollStrike()
+        {
+            _game.Roll(10);
+        }
+        private void RollSpare()
+        {
+            _game.Roll(5);
+            _game.Roll(5);
+        }
+
+        [Test]
+        public void Can_Get_Calculate_Full_Game_Scores()
+        {
+            for (int i = 0; i < 12; i++)
+                RollStrike();
+            Console.WriteLine("Roll Total - {0}, Result - {1}", 300, _game.Score());
+            Assert.That(300, Is.EqualTo(_game.Score()));
+        }
+
+
+        [Test]
+        public void Can_Get_Calculate_Scores()
+        {
+            for (int i = 0; i < 20; i++)
+                _game.Roll(0);
+            Assert.That(0, Is.EqualTo(_game.Score()));
+        }
+
+        [Test]
+        public void Can_Get_Calculate_Single_Scores()
+        {
+            _game.Roll(7);
+            Console.WriteLine("Roll Total - {0}, Result - {1}", 7, _game.Score());
+            Assert.That(7, Is.EqualTo(_game.Score()));
+        }
+        [Test]
+        public void Can_Get_Calculate_SingleFrame_Scores()
+        {
+            _game.Roll(7);
+            _game.Roll(2);
+            Console.WriteLine("Roll Total - {0}, Result - {1}", 9, _game.Score());
+            Assert.That(9, Is.EqualTo(_game.Score()));
+        }
+
+
+        [Test]
+        public void Can_Get_Calculate_Spare_Scores()
+        {
+            RollSpare();
+            _game.Roll(3);
+            _game.Roll(2);
+            for (int i = 0; i < 16; i++)
+                _game.Roll(0);
+            Console.WriteLine("Roll Total - {0}, Result - {1}", 18, _game.Score());
+            Assert.That(18, Is.EqualTo(_game.Score()));
+        }
+
+        [Test]
+        public void Can_Get_Calculate_Strike_Scores()
+        {
+            RollStrike();
+            _game.Roll(3);
+            _game.Roll(4);
+            _game.Roll(2);
+            for (int i = 0; i < 15; i++)
+                _game.Roll(0);
+            Console.WriteLine("Roll Total - {0}, Result - {1}", 26, _game.Score());
+            Assert.That(26, Is.EqualTo(_game.Score()));
+        }
+        [Test]
+        public void Can_Get_Calculate_2Strikes_Score()
+        {
+            RollStrike();
+            RollStrike();
+            _game.Roll(3);
+            _game.Roll(4);
+            for (int i = 0; i < 14; i++)
+                _game.Roll(0);
+            Console.WriteLine("Roll Total - {0}, Result - {1}", 47, _game.Score());
+            Assert.That(47, Is.EqualTo(_game.Score()));
+        }
+        [Test]
+        public void Can_Get_Calculate_2Spare_Score()
+        {
+            RollSpare();
+            _game.Roll(3);
+            _game.Roll(7);
+            _game.Roll(6);
+            _game.Roll(1);
+            for (int i = 0; i < 14; i++)
+                _game.Roll(0);
+            Console.WriteLine("Roll Total - {0}, Result - {1}", 36, _game.Score());
+            Assert.That(36, Is.EqualTo(_game.Score()));
+        }
+        [Test]
+        public void Can_Get_Calculate_3Strikes_Score()
+        {
+            RollStrike();
+            RollStrike();
+            RollStrike();
+            _game.Roll(3);
+            _game.Roll(4);
+            for (int i = 0; i < 12; i++)
+                _game.Roll(0);
+            Console.WriteLine("Roll Total - {0}, Result - {1}", 77, _game.Score());
+            Assert.That(77, Is.EqualTo(_game.Score()));
+        }
+        [Test]
+        public void Can_Get_Calculate_2Strikes1Spare_Score()
+        {
+            RollStrike();
+            RollStrike();             
+            _game.Roll(4);
+            _game.Roll(6);
+            _game.Roll(3);
+            for (int i = 0; i < 13; i++)
+                _game.Roll(0);
+            Console.WriteLine("Roll Total - {0}, Result - {1}", 60, _game.Score());
+            Assert.That(60, Is.EqualTo(_game.Score()));
+        }
+        [Test]
+        public void Can_Get_Calculate_1Spare_LastFrame_Score()
+        {
+            for (int i = 0; i < 18; i++)
+                _game.Roll(0);
+            RollSpare();
+            _game.Roll(3);
+            Console.WriteLine("Roll Total - {0}, Result - {1}", 13, _game.Score());
+            Assert.That(13, Is.EqualTo(_game.Score()));
+
+        }
+        [Test]
+        public void Can_Get_Calculate_3Strikes_LastFrame_Score()
+        {
+            for (int i = 0; i < 18; i++)
+                _game.Roll(0);
+            RollStrike();
+            RollStrike();
+            RollStrike();
+            Console.WriteLine("Roll Total - {0}, Result - {1}", 60, _game.Score());
+            Assert.That(30, Is.EqualTo(_game.Score()));
+
+        }
+    }
+}
---
+++ PrimeService/BowlingGame.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prime.Services
+{
+//    The game consists of 10 frames, in each frame the player has the ability to knock down 10 pins.
+//    The score for the frame is the total number of pins knocked down + bonuses for strikes and spares.
+//    A spare is when the player knocks down all 10 pins in 2 tries.The bonus for a spare is the next roll.
+//    A strike is when the player knocks down all 10 pins in 1 try. The bonus is the next 2 rolls.
+//    In the tenth frame a player who rolls a spare / strike gets an extra roll(s) to complete the frame.
+//    No more than 3 rolls can be rolled in the 10th frame.
+    public class BowlingGame
+    {
+        private readonly int[] _rolls = new int[21];
+        private int _currentRoll;
+
+        public void Roll(int pins) => _rolls[_currentRoll++] = pins;
+
+        public int Score()
+        {
+            var score = 0;
+            var frameIndex = 0;
+            for (var frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(frameIndex))
+                {
+                    score += StrikeBonus(frameIndex);
+                    frameIndex++;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    score += 10 + SpareBonus(frameIndex);
+                    frameIndex += 2;
+                }
+
+                else
+                {
+                    score += SumOfBallsInFrames(frameIndex);
+                    frameIndex += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private bool IsStrike(int frameIndex) => _rolls[frameIndex] == 10;
+
+        private int SumOfBallsInFrames(int frameIndex) => _rolls[frameIndex] + _rolls[frameIndex + 1];
+
+        private int SpareBonus(int frameIndex) => _rolls[frameIndex + 2];
+
+        private bool IsSpare(int frameIndex) => _rolls[frameIndex] + _rolls[frameIndex + 1] == 10;
+
+        private int StrikeBonus(int frameIndex) => 10 + _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+
+
+    }
+}
